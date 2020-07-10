451b387eb740fa6ea15bee55f42e0ca2d1f4bbff
Travis Cultreri <travis@Travis-X.local>
2020-07-10 17:56:06 -0400

    removed unused components from app js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import Wrapper from "./components/wrapper/Wrapper"
-// import logo from './logo.svg';
-import Header from "./components/header/Header"
-import EmployeeTable from "./components/table/EmployeeTable"
 import './App.css';
-import API from "../src/utils/API"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
