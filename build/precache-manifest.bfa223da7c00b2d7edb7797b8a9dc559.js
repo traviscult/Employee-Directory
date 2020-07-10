67b0474fa133175359f82bf7e3c21cda7b94b3a1
Travis Cultreri <travis@Travis-X.local>
2020-07-10 17:54:13 -0400

    removed unused searchbar from header js to deploy

---
+++ build/precache-manifest.bfa223da7c00b2d7edb7797b8a9dc559.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6926c938df3cd5e4c5e6dd4ac7021d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38b2a00a5cc549f40767",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a7679c0582ebdb46c6ce",
+    "url": "/static/css/main.94002fef.chunk.css"
+  },
+  {
+    "revision": "38b2a00a5cc549f40767",
+    "url": "/static/js/2.e8b64b22.chunk.js"
+  },
+  {
+    "revision": "d72c695bb2fed22c8cbf125cbcb911ef",
+    "url": "/static/js/2.e8b64b22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7679c0582ebdb46c6ce",
+    "url": "/static/js/main.3828d2e9.chunk.js"
+  },
+  {
+    "revision": "c74b6dcb725f329d7bfe",
+    "url": "/static/js/runtime-main.403e0656.js"
+  }
+]);
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,4 @@
 import React from "react";
-import SearchBar from "../searchBar/SearchBar";
 import "./style.css"
 
 
@@ -9,8 +8,6 @@ const Header = () => {
         <div className="header">
            <h1>Employee Directory</h1>
            <p>Click on carrots to filter by heading or use the search box to narrow your results.</p>
-           {/* <SearchBar
-           /> */}
         </div>
     )
 }
